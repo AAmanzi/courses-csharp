b39600f05b09eaccc2d56808285ee8d840d6935a
AAmanzi <alnick.amanzi@gmail.com>
2020-03-06 15:56:40 +0100

    add(excercise-2): setup and Triangle class

---
+++ excercise-2/task-1/Shape.cs
@@ -0,0 +1,22 @@
+namespace task_1
+{
+	public abstract class Shape
+	{
+		protected double xPos;
+		protected double yPos;
+
+		public abstract double getArea();
+		public abstract double getPerimeter();		
+		public abstract void printData();
+
+		public double getXPos()
+		{
+			return xPos;
+		}
+
+		public double getYPos()
+		{
+			return yPos;
+		}
+	}
+}
